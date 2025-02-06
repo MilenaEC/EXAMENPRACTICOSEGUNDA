c9a550769bb750932c4545b7504c413d5aa54f26
MilenaEC <milena.cabascango695@ist17dejulio.edu.ec>
2025-02-06 16:29:34 -0500

    Inicio del examen

---
+++ src/main/java/com/mycompany/examenpracticosegunda/EXAMENPRACTICOSEGUNDA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examenpracticosegunda;
+
+/**
+ *
+ * @author ASUS
+ */
+public class EXAMENPRACTICOSEGUNDA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
